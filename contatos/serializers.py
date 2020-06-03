adf41e925d8a7144186c6bed9adcef2fc54eca3b
Tiago <tiagolch@hotmail.com>
2020-06-03 20:40:24 -0300

    refatorado serializers

--- contatos/serializers.py
+++ contatos/serializers.py
@@ -4,14 +4,12 @@ from rest_framework import serializers
 class empresaSerializer(serializers.ModelSerializer):
     class Meta:
         model = empresa
-        fields = ['id', 'nome_empresa']
-
+        fields = '__all__'
 
 class setorSerializer(serializers.ModelSerializer):
     class Meta:
         model = setor
-        fields = ['id', 'nome_setor']
-
+        fields = '__all__'
 
 class contatoSerializer(serializers.ModelSerializer):
     nome_empresa = serializers.CharField(source='empresa.nome_empresa')  ## necessario porque é um campo Foreign Key
--- contatos/views.py
+++ contatos/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from contatos.models import contato
 from contatos.serializers import contatoSerializer
-from rest_framework import generics
 
 
 class contatoViewSet(viewsets.ModelViewSet):
